4d28da508332d77a7a5ece048ef763220350db09
wjhwjh <1468017149@qq.com>
2019-11-20 14:31:00 +0800

    提交

---
+++ module/defer.js
@@ -0,0 +1,9 @@
+function foo() {
+    console.log('我是一个函数')
+}
+
+let a = 'huizi';
+
+
+foo();
+console.log(a);
---
+++ module/defer1.js
@@ -0,0 +1,4 @@
+let box = document.getElementById('box');
+
+console.log(box);
+console.log(box.style);
